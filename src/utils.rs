f10fdea28dd2fbfa3220d9beb8d17c026014e179
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-10 11:14:33 +0800

    Remove unneccessary to_string

--- src/utils.rs
+++ src/utils.rs
@@ -7,7 +7,6 @@ pub fn format_price(price: f64) -> String {
         price if price > 1_000_000.0 => format!("${:.2}M", price / 1_000_000.0),
         price if price > 100_000.0 => format!("${:.2}K", price / 1_000.0),
         price if price > 1.0 => format!("${:.2}", price),
-        _ => format!("${}", price.to_precision(5).to_string()),
+        _ => format!("${}", price.to_precision(5)),
     }
 }
-
